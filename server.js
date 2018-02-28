954146708945b5a566ddac7b9a81280ce31ee585
Manjula96 <36848091+Manjula96@users.noreply.github.com>
2018-02-28 12:16:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -70,9 +70,9 @@ var temp=`
 return temp;
 }
 
-//app.get('/', function (req, res) {
-  //res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-//});
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
 
 app.get('/:articleName', function (req, res){
     var articleName=req.params.articleName;
