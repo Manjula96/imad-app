1f44f850678d88dc44a91816a135c11059604473
Manjula96 <36848091+Manjula96@users.noreply.github.com>
2018-02-28 12:27:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -74,15 +74,17 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/:articleName', function (req, res){
-    var articleName=req.params.articleName;
-    res.send(createTemp(articles[articleName]));
-});
-
 app.get('/counter',function (req, res){
     counter=counter+1;
     res.send(counter.toString());
 });
+
+app.get('article/:articleName', function (req, res){
+    var articleName=req.params.articleName;
+    res.send(createTemp(articles[articleName]));
+});
+
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
