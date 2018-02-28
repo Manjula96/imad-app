ff03d56385a1595c944c6982998efeb6e73f7b3b
Manjula96 <36848091+Manjula96@users.noreply.github.com>
2018-02-28 08:51:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,12 +4,13 @@ var ele=document.getElementById('txt');
 ele.innerHTML='Changed to new one';
 
 var img=document.getElementById('im1');
-var mar=0;
-function move()
-{
-    mar=mar+10;
-    img.style.mar=mar+'px';
-}
+//var mar=0;
+//function move()
+//{
+   // mar=mar+10;
+   // img.style.mar=mar+'px';
+//}
 img.onclick=function(){
-    var interval=setInterval(move,100);
+    img.style.mar='100px';
+    //var interval=setInterval(move,100);
 };
