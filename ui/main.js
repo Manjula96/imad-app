d0130282b5763ab5836372ae6923117ace4b5f0a
Manjula96 <36848091+Manjula96@users.noreply.github.com>
2018-02-28 08:06:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,14 @@ console.log('Loaded!');
 
 var ele=document.getElementById('txt');
 ele.innerHTML='Changed to new one';
+
+var img=document.getElementById('im1');
+var mar=0;
+function move()
+{
+    mar=mar+10;
+    img.style.mar=mar+'px';
+}
+img.onclick=function(){
+    var interval=setInterval(move,100);
+};
